c00e5484e5ef323882e8e8295609cd1643f6dd59
Brunoo-G <bruno_nicolas88@hotmail.com>
2022-04-04 17:03:40 -0300

    Ruta de imagenes

--- movitech/db/index.js
+++ movitech/db/index.js
@@ -3,81 +3,85 @@ let perfil = {email: 'bobbyfischer@gmail.com' ,
     contraseña: 'p4wnE4' ,
     nacimiento: '09/03/1943' ,
     documento: 15738990 ,
-    foto: '../fotos/perfilUsuario.jpg' ,
-    }
-
+    foto: '/images/perfilUsuario.jpg' ,
+};
 
 let comentarios = [
     {nombre:'C0mentaristaEnojado' , 
     texto:'Pésimo servicio, la última vez que use este celular cargando explotó y me quemó al mitad de la cara.' , 
-    imagen: '../fotos/perfilComentario.jpg' },
+    imagen: '/images/perfilComentario.jpg' },
 
     {nombre:'JorgePerezzz' , 
     texto:'100% recomendado, el mejor celular que usé en mi vida.' , 
-    imagen: '../fotos/perfilComentario.jpg' },
+    imagen: '/images/perfilComentario.jpg' },
 
     {nombre:'DIBenede' , 
     texto:'3/5 estrellas, mi hijo (2 años) mordió la pantalla y ahora no funciona' , 
-    imagen: '../fotos/perfilComentario.jpg' },
+    imagen: '/images/perfilComentario.jpg' },
 
     {nombre:'Julian.ferreyra' , 
     texto:'Genial, lo uso todos los días hace 4 años y nunca falla.' , 
-    imagen: '../fotos/perfilComentario.jpg' },
+    imagen: '/images/perfilComentario.jpg' },
 
     {nombre:'Pedro_ramos' , 
     texto:'Es tan malo que cualquier palabra que use para describirlo es un insulto a esa palabra' , 
-    imagen: '../fotos/perfilComentario.jpg' },
-]
+    imagen: '/images/perfilComentario.jpg' },
+];
 
 let productos = [
-    {imagen:'../fotos/productos/A52.jpg' , 
+    {imagen:'/images/productos/A52.jpg' , 
     nombre:'A52' , 
     descripcion:'Smartphone Samsung Galaxy A52: Procesador Snapdragon 720G Octa Core (hasta 2.3 GHz), Memoria RAM de 8GB, Almacenamiento de 128GB, Pantalla Super AMOLED de 6.3" FHD+, Red, Bluetooth, Wi-Fi, Cámara principal de 64MP, Android 11.' , 
     fechaCarga:'02/11/21' },
 
-    {imagen:'../fotos/productos/Redmi10.jpg' , 
+    {imagen:'/images/productos/Redmi10.jpg' , 
     nombre:'Redmi10' , 
     descripcion:'Smartphone Xiaomi Redmi Note 10 5G: Procesador MediaTek Dimensity 700 (hasta 2.20 GHz) Memoria RAM de 6GB, Almacenamiento de 128GB, Pantalla LED Multi Touch de 6.5" FHD+, Bluetooth 5.1, Wi-Fi 5, Cámara Principal de 48MP, Android 11.' , 
     fechaCarga:'05/06/20' },
 
-    {imagen:'../fotos/productos/MotoG50' , 
+    {imagen:'/images/productos/MotoG50' , 
     nombre:'Moto G50' , 
     descripcion:'Smartphone Motorola Moto G50 5G: Procesador Qualcomm Snapdragon 480 (hasta 2.00 GHz), Memoria RAM de 4GB, Almacenamiento de 128GB, Pantalla LED Multi Touch de 6.5" HD+, Bluetooth 5.0, Wi-Fi, Cámara Principal de 48MP, Android 11.' , 
     fechaCarga:'22/04/19' },
 
-    {imagen:'../fotos/productos/MotoE20.jpg' , 
+    {imagen:'/images/productos/MotoE20.jpg' , 
     nombre:'Moto E20' , 
     descripcion:'Smartphone Motorola Moto E20: Procesador Unisoc T606 Octa Core (1.6 GHz), Memoria RAM de 2GB, Almacenamiento de 32GB, Pantalla LED Multi Touch de 6.5" HD+, Bluetooth, Wi-Fi, Cámara principal de 13MP, Android 11. ' , 
     fechaCarga:'15/11/18' },
 
-    {imagen:'../fotos/productos/GalaxyA12.jpg' , 
+    {imagen:'/images/productos/GalaxyA12.jpg' , 
     nombre:'Galaxy A12' , 
     descripcion:'Smartphone Samsung Galaxy A12: Procesador Octa-Core (hasta 2.3 GHz), Memoria RAM de 4GB, Almacenamiento de 64GB, Pantalla LED Multi Touch de 6.4", Bluetooth, Wi-Fi, Cámara principal de 48MP, Android 10.' , 
     fechaCarga:'19/08/20' },
 
-    {imagen:'../fotos/productos/MotoE7i.jpg' , 
+    {imagen:'/images/productos/MotoE7i.jpg' , 
     nombre:'Moto E7i' , 
     descripcion:'Smartphone Motorola Moto E7i Power: Procesador Unisoc SC9863A Octa-Core (hasta 1.60 GHz), Memoria RAM de 2GB, Almacenamiento de 32GB, Pantalla LED Multi Touch de 6.5", Bluetooth, Wi-Fi, Cámara Principal de 13MP, Android 10.' , 
     fechaCarga:'09/10/20' },
 
-    {imagen:'../fotos/productos/GalaxyS20.jpg' , 
+    {imagen:'/images/productos/GalaxyS20.jpg' , 
     nombre:'Galaxy S20' , 
     descripcion:'Smartphone Samsung Galaxy S20 FE: Procesador Snapdragon 865 5G (Hasta 2.8 GHz), Memoria RAM de 6GB, Almacenamiento de 128GB, Pantalla Super AMOLED de 6.3" FHD+, Batería 4500 mAh, Bluetooth, Wi-Fi, Cámara Principal de 12MP, Android 10.' , 
     fechaCarga:'11/01/22' },
 
-    {imagen:'../fotos/productos/GalaxyA22.jpg' , 
+    {imagen:'/images/productos/GalaxyA22.jpg' , 
     nombre:'Galaxy A22' , 
     descripcion:'Smartphone Samsung Galaxy A22: Procesador MediaTek Helio G80, Memoria RAM de 4GB, Almacenamiento de 128GB, Pantalla LED de 6.4", Bluetooth 5.0, Wi-Fi, Cámara principal de 48MP, Android 11.' , 
     fechaCarga:'28/12/20' },
 
-    {imagen:'../fotos/productos/GalaxyA32.jpg' , 
+    {imagen:'/images/productos/GalaxyA32.jpg' , 
     nombre:'Galaxy A32' , 
     descripcion:'Smartphone Samsung Galaxy A32: Procesador Mediatek Helio G80 Octa Core (hasta 2.0 GHz), Memoria RAM de 4GB, Almacenamiento de 128GB, Pantalla Super AMOLED de 6.2" FHD+, Bluetooth 5.0, Wi-Fi 5, Cámara principal de 64MP, Android 11.' , 
     fechaCarga:'20/10/21' },
 
-    {imagen:'../fotos/productos/iPhoneX.jpg' , 
+    {imagen:'/images/productos/iPhoneX.jpg' , 
     nombre:'iPhone X' , 
     descripcion:'Apple iPhone X: Procesador A11 Bionic, Memoria RAM de 3GB, Almacenamiento de 64GB, Pantalla OLED de 5.8" Super Retina HD, Bluetooth 5.0, Wi-Fi, 4G, Cámara principal de 12 MP, iOS 11. ' , 
     fechaCarga:'30/01/19' },
-]
+];
 
+module.exports = {
+    perfil,
+    comentarios,
+    productos
+};
