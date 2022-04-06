2b301e38cad8dfa081d5956eb189497de88aabc1
JoaquinMendivil <jmendivil@udesa.edu.ar>
2022-04-06 16:25:14 -0300

    Corrijo errores

---
+++ movitech/routes/products.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/agregarProducto', function(req, res, next) {
+    res.send('holaaa');
+});
+
+router.get('/detalleProducto', function(req, res, next) {
+  res.send('holaaa');
+});
+
+module.exports = router;
