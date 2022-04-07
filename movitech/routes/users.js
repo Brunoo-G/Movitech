2e0ec7870b6814596d573e91c99a36e524549360
JoaquinMendivil <jmendivil@udesa.edu.ar>
2022-04-07 15:38:26 -0300

    Cambio nombre perfil

--- movitech/routes/users.js
+++ movitech/routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var controller = require('../controllers/userController')
 
 
-router.get('/me', controller.profile) ;
+router.get('/profile', controller.profile) ;
 
-router.get('/me/edit', controller.profile) ;
+router.get('/profile/edit', controller.profile) ;
 
 module.exports = router;
