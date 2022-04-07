9b5dda433019d447f07e166c38461b5a4e39ba93
JoaquinMendivil <jmendivil@udesa.edu.ar>
2022-04-07 17:33:23 -0300

    Perfil ejs y cambio controlador usuario

--- movitech/controllers/userController.js
+++ movitech/controllers/userController.js
@@ -3,7 +3,14 @@ var data = require("../db/index")
 const controller = {
     profile: function(req, res) {
         res.render('profile', { perfil: data.usuario});
+    },
+
+    profileEdit: function(req, res) {
+            res.render('profile-edit', { perfil: data.usuario});
+        }
     }
-}
+
+
+
 
 module.exports = controller;
